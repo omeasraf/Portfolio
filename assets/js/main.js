e872289a3bb07aff0d20e99887f4444d78399bd3
Ome Asraf <omeasraf0@gmail.com>
2021-04-12 12:16:59 -0400

    Added a loading bar

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,16 @@ function scrollToID(idName){
         behavior: 'smooth'
       });
 }
+
+$(document).ready(function() {
+	$(window).bind('load', function() {
+		$('.overlay, body').addClass('loaded');
+		setTimeout(function() {
+			$('.overlay').css({'display':'none'})
+		}, 2000)
+	});
+	
+	setTimeout(function() {
+		$('.overlay, body').addClass('loaded');
+	}, 50000);
+})
