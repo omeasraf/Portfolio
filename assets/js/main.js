9d447a244a4a982467d1f28f909ed614e55b092a
Ome Asraf <omeasraf0@gmail.com>
2021-04-12 12:29:44 -0400

    Added a pulsing effect
    Added a pulsing effect to elements on click and scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,25 @@
-function scrollToID(idName){
+function scrollToID(idName) {
+    $("#" + idName).addClass('pulse');
     document.getElementById(idName).scrollIntoView({
         behavior: 'smooth'
-      });
+    });
+    
+    setTimeout(function () {
+        $("#" + idName).removeClass('pulse');
+    }, 3000);
 }
 
-$(document).ready(function() {
-	$(window).bind('load', function() {
-		$('.overlay, body').addClass('loaded');
-		setTimeout(function() {
-			$('.overlay').css({'display':'none'})
-		}, 2000)
-	});
-	
-	setTimeout(function() {
-		$('.overlay, body').addClass('loaded');
-	}, 50000);
+$(document).ready(function () {
+    $(window).bind('load', function () {
+        $('.overlay, body').addClass('loaded');
+        setTimeout(function () {
+            $('.overlay').css({
+                'display': 'none'
+            })
+        }, 2000)
+    });
+
+    setTimeout(function () {
+        $('.overlay, body').addClass('loaded');
+    }, 50000);
 })
