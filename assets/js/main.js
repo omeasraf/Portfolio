977422f6867db180ec4b3ab915f8ce665e58aba8
Ome Asraf <omeasraf0@gmail.com>
2021-04-12 15:52:23 -0400

    Removed loading indicator

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,18 +8,3 @@ function scrollToID(idName) {
         $("#" + idName).removeClass('pulse');
     }, 3000);
 }
-
-$(document).ready(function () {
-    $(window).bind('load', function () {
-        $('.overlay, body').addClass('loaded');
-        setTimeout(function () {
-            $('.overlay').css({
-                'display': 'none'
-            })
-        }, 2000)
-    });
-
-    setTimeout(function () {
-        $('.overlay, body').addClass('loaded');
-    }, 50000);
-})
